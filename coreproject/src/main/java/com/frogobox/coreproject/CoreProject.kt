4eefd2cc734603c4a40f325176bacb9becce6c0b
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-04-17 08:35:41 +0700

    update: bug test

---
+++ coreproject/src/main/java/com/frogobox/coreproject/CoreProject.kt
@@ -0,0 +1,17 @@
+package com.frogobox.coreproject
+
+
+/*
+ * Created by faisalamir on 17/04/22
+ * JuaraAndroid
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2022 Frogobox Media Inc.      
+ * All rights reserved
+ *
+ */
+ 
+ 
